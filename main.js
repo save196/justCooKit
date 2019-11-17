a7bdff62f1bae099615fa08d1a8de25f99178941
filippopiconese <filippopiconese@hotmail.it>
2019-11-17 05:16:29 +0200

    Boh

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const getRecipes = require('./apiModules/getRecipes')
-const { getIngredients, getRecipePrice } = require('./utils')
+const { getIngredients, getVanillaIngredients, getRecipePrice } = require('./utils')
 
 recipesURL = process.env.RECIPES_API_URL
 
@@ -10,9 +10,10 @@ async function main() {
   let requiredPortions = 1
   recipes = await getRecipes(limit, specialDiet, maxTime)
   ingredients = getIngredients(recipes.results[0], requiredPortions)
+  // vanillaIngredients = getVanillaIngredients(recipes.results[0], requiredPortions)
 
   // console.log(JSON.stringify(recipes.results[0], null, 4))
-  // console.log(JSON.stringify(ingredients, null, 4))
+  // console.log(JSON.stringify(vanillaIngredients, null, 4))
 
   let price = await getRecipePrice(ingredients)
 
